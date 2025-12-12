b5a17eae6cae7208128902886a9420c9bc86cded
dlg383 <dlg383@inlumine.ual.es>
2025-12-12 21:10:17 +0100

    Dockerizar

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const dictionaryRoutes = require('./routes/dictionary.routes');
 const playersRoutes = require('./routes/players.routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
